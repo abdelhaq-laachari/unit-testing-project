112cf5b3de91ffd477241bd6ec5602db44bed470
Abdelhaq laachari <93976987+abdelhaq-laachari@users.noreply.github.com>
2023-01-16 09:59:00 +0100

    first test

---
+++ function.test.js
@@ -0,0 +1,9 @@
+const functions = require("./function");
+
+test("Adds 2 + 2 to equal 4", () => {
+  expect(functions.add(2, 2)).toBe(4);
+});
+
+test("Adds 2 + 2 to NOT equal 4", () => {
+  expect(functions.add(2, 2)).not.toBe(5);
+});
